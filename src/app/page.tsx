3a4ac28ff44a668821b1b3bba0a28757ff6db26d
Aryan Maskara <aryan.maskara@gmail.com>
2024-05-18 17:19:54 +0530

    adjusted title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      
+    <main className="flex min-h-screen items-center">
       <div className="background-container">
-  <div className="content">
-    <div className="main-heading">
-      
-        <div className="flex-auto heading-flex">
-          <img src="/ots.svg" alt="Open Tech Summit Logo"/>
-          <h1 className="text-2xl">Open Tech Summit</h1>
+        <div className="content flex justify-center w-full">
+          <div className="main-heading text-center w-full flex items-center justify-center mt-8">
+            <div className="heading-flex flex items-center justify-center">
+              <img src="/ots.svg" alt="Open Tech Summit Logo" className="w-24 h-24 -mr-2" />
+              <h1 className="text-2xl md:text-5xl font-bold ml-4">Open Tech Summit</h1>
+            </div>
+          </div>
         </div>
-      
-    </div>
-  </div>
-</div>
-
-      
-
-      
+      </div>
     </main>
   );
 }
