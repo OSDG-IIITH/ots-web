3fdc4a06c0b7e4c9ed55e69b63b52d362ebd6a7f
Aryan Maskara <aryan.maskara@gmail.com>
2024-05-20 05:32:09 +0530

    refactor: made background separate from foreground

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import './globals.css';
 
 export default function Home() {
   const [countdown, setCountdown] = useState({ days: 0, hours: 0 });
@@ -42,57 +43,58 @@ export default function Home() {
     return () => clearInterval(interval);
   }, []); // Run this effect only once on component mount
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex items-center justify-center">
       <div className="background-container w-full">
-        <div className="content flex flex-col items-center justify-center w-full " style={{ padding: '5%' }}>
-          <div className="main-heading text-center flex items-center justify-center mt-8">
-            <div className="heading-media heading-flex flex items-center justify-center mt-8" >
-              <img src="/ots.svg" alt="Open Tech Summit Logo" className=" responsive-image max-w-full max-h-full" />
-              <h1 className="text-5xl md:text-7xl font-bold ml-4">Open Tech Summit</h1>
+      </div>
+        <div className="content flex flex-col items-center justify-center w-full">
+          <div className="main-heading text-center flex flex-col items-center justify-center">
+            <div className="heading-media heading-flex flex items-center justify-center">
+              <img
+                src="/logo.svg"
+                alt="Open Tech Summit Logo"
+                className="responsive-image max-w-[25%] max-h-[25%]"
+              />
+              <h1 className="text-3xl md:text-7xl font-bold">Open Tech Summit</h1>
             </div>
           </div>
           {/* Countdown Timer */}
-          <div className="flex flex-col justify-center items-center bg-[#131415] px-10 py-6 rounded-2xl shadow-lg mt-12">
+          <div className="flex flex-col justify-center items-center bg-[#131415] px-10 max-w-[75%] py-6 rounded-2xl shadow-lg mt-8">
             {/* Date */}
-            <h1 className="text-center pt-6 mx-10">
-              <span className="bg-white text-4xl font-bold text-transparent bg-clip-text">11</span>
-              <span className="bg-white text-4xl text-transparent bg-clip-text">th</span>
-              <span className="bg-white text-4xl font-bold text-transparent bg-clip-text"> to 13</span>
-              <span className="bg-white text-4xl text-transparent bg-clip-text">th</span>
-              <span className="bg-gradient-to-r from-white to-[#80d8de] text-4xl font-bold text-transparent bg-clip-text"> September</span>
+            <h1 className="text-center md:pt-6">
+              <span className="enlarge-text bg-white text-2xl md:text-4xl font-bold text-transparent bg-clip-text">11</span>
+              <span className="enlarge-text bg-white text-2xl md:text-4xl text-transparent bg-clip-text">th</span>
+              <span className="enlarge-text bg-white text-2xl md:text-4xl font-bold text-transparent bg-clip-text"> to 13</span>
+              <span className="enlarge-text bg-white text-2xl md:text-4xl text-transparent bg-clip-text">th</span>
+              <span className="enlarge-text bg-gradient-to-r from-white to-[#80d8de] text-2xl md:text-4xl font-bold text-transparent bg-clip-text"> September</span>
             </h1>
             {/* Countdown */}
             <div className="flex flex-row items-center justify-evenly text-transparent">
-              <div className="bg-[#18181c] flex flex-col rounded-2xl items-center justify-center mt-10 ml-5 mr-6 mb-10 px-10 pt-5 pb-2">
+              <div className="bg-[#18181c] flex flex-col rounded-2xl items-center justify-center mr-2 md:mt-10 md:ml-5 md:mr-6 md:px-10 md:pt-5">
                 <div className="bg-gradient-to-b from-white to-[#80d8de] text-transparent bg-clip-text flex flex-col items-center justify-center">
-                  <span className="my-2 mx-5 text-5xl font-bold mb-2"> {countdown.days} </span>
-                  <span className="my-2 mx-5 text-lg font-bold mb-2"> Days </span>
+                  <span className="my-2 mx-5 text-2xl md:text-5xl font-bold mb-2"> {countdown.days} </span>
+                  <span className="my-2 mx-5 text-base md:text-2xl font-bold mb-2"> Days </span>
                 </div>
               </div>
-              <div className="bg-[#18181c] flex flex-col rounded-2xl items-center justify-center mt-10 ml-6 mr-5 mb-10 px-10 pt-5 pb-2">
+              <div className="bg-[#18181c] flex flex-col rounded-2xl items-center justify-center ml-2 md:mt-10 md:ml-5 md:mr-6 md:px-10 md:pt-5">
                 <div className="bg-gradient-to-b from-white to-[#80d8de] text-transparent bg-clip-text flex flex-col items-center justify-center">
-                  <span className="my-2 mx-5 text-5xl font-bold mb-2"> {countdown.hours} </span>
-                  <span className="my-2 mx-5 text-lg font-bold mb-2"> Hours </span>
+                  <span className="my-2 mx-5 text-2xl md:text-5xl font-bold mb-2"> {countdown.hours} </span>
+                  <span className="my-2 mx-5 text-base md:text-2xl font-bold mb-2"> Hours </span>
                 </div>
               </div>
             </div>
             {/* Keep Me Updated Button */}
-            
-              <div className="flex flex-col items-center justify-center mt-5">
-                <div className="bg-gradient-to-r from-[#4fd4cd] to-[#275fe8] text-white rounded-lg mb-8">
-                <a
-              href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=OTS&details=Open Tech Summit 2024 IIIT-H&dates=20240911T103000/20240913T184500&location=IIIT Hyderabad"
-              className="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block bg-gradient-to-r from-[#4fd4cd] to-[#275fe8] text-white"
-            >
-              <p className="px-5 py-2 text-base">Keep me updated</p>
-            </a>
-                </div>
-
+            <div className="flex flex-col items-center justify-center mt-5">
+              <div className="bg-gradient-to-r from-[#4fd4cd] to-[#275fe8] text-white rounded-lg md:mt-4 mb-8">
+                {/* <a
+                  href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=OTS&details=Open Tech Summit 2024 IIIT-H&dates=20240911T103000/20240913T184500&location=IIIT Hyderabad"
+                  className="hover:bg-blue-700 text-white font-bold py-2 md:px-4 rounded inline-block bg-gradient-to-r from-[#4fd4cd] to-[#275fe8] text-white"
+                > */}
+                  <p className="px-5 py-2 text-sm md:text-2xl rounded-xl">Keep me updated</p>
+                {/* </a> */}
               </div>
-            
+            </div>
           </div>
         </div>
-      </div>
     </main>
   );
 }
