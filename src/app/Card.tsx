5809740dd54ce0b124052ec27927551e3fd30d9c
PraneethJain <praneethjain005@gmail.com>
2024-05-27 20:44:36 +0530

    feat: events content

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, club, description, icon }) => {
   return (
-    <div key={title} className="bg-[#1e1e24] text-white p-4 rounded-lg shadow-md m-2 flex flex-col items-center">
+    <div key={title} className="bg-[#1e1e24] text-white p-4 rounded-lg shadow-md m-2 flex flex-col items-center text-center">
       <div className="flex items-center justify-center w-16 h-16 bg-[#333333] rounded-xl mb-4">
         <img src={icon} alt={`${title} Icon`} className="w-12 h-12" />
       </div>
